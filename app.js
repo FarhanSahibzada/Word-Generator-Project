a0e07675ce741c16defeb5a09c904b304edded99
Farhan Sahibzada <154471620+FarhanSahibzada@users.noreply.github.com>
2024-06-30 17:00:32 +0500

    Word Generator..
    A simple word generator. In this word generator, if you click any alphabet, it will generate a word starting with that letter. If you type any letter with your keyboard, it will work the same way. Additionally, if you click any letter, the word will be displayed, and the program will provide information about the word.

---
+++ app.js
@@ -0,0 +1,288 @@
+
+
+// function play(alpha) {
+
+
+
+//     if (alpha === "A") {
+
+//         let arrA = ["Apple", "Aeroplane", "Anger", "Alarm", "Actor"];
+//         let num = Math.floor(Math.random() * 5);
+
+//         document.getElementById("display").innerHTML = "= " + arrA[num]
+//     }
+//     else if (alpha === "B") {
+
+//         let arr = ["banana", "basket", "bird", "book", "butterfly"];
+//         let num = Math.floor(Math.random() * 5);
+
+//         document.getElementById("displayB").innerHTML = "= " + arr[num]
+//     }
+//     else if (alpha === "C") {
+
+//         let arr = ["cat", "car", "cake", "cookie", "cloud"];
+//         let num = Math.floor(Math.random() * 5);
+
+//         document.getElementById("displayC").innerHTML = "= " + arr[num]
+//     }
+//     else if (alpha === "D") {
+
+//         let arr = ["dog", "duck", "door", "diamond", "dragon"];
+
+//         let num = Math.floor(Math.random() * 5);
+
+//         document.getElementById("displayD").innerHTML = "= " + arr[num]
+//     }
+//     else if (alpha === "E") {
+
+//         let arr = ["elephant", "egg", "earth", "eagle", "engine"];
+
+//         let num = Math.floor(Math.random() * 5);
+
+//         document.getElementById("displayE").innerHTML = "= " + arr[num]
+//     }
+//     else if (alpha === "F") {
+
+//         let arr = ["fish", "flower", "fire", "flag", "forest"];
+//         let num = Math.floor(Math.random() * 5);
+
+//         document.getElementById("displayF").innerHTML = "= " + arr[num]
+//     }
+//     else if (alpha === "G") {
+
+//         let arr = ["goat", "grass", "glass", "guitar", "gift"];
+
+//         let num = Math.floor(Math.random() * 5);
+
+//         document.getElementById("displayG").innerHTML = "= " + arr[num]
+//     }
+//     else if (alpha === "H") {
+
+//         let arr = ["horse", "hat", "house", "heart", "honey"];
+//         let num = Math.floor(Math.random() * 5);
+
+//         document.getElementById("displayH").innerHTML = "= " + arr[num]
+//     }
+//     else if (alpha === "I") {
+
+//         let arr = ["ice cream", "island", "igloo", "ink", "iron"];
+//         let num = Math.floor(Math.random() * 5);
+
+//         document.getElementById("displayI").innerHTML = "= " + arr[num]
+//     }
+//     else if (alpha === "J") {
+
+
+//         let arr = ["jellyfish", "jacket", "juice", "jam", "jet"];
+//         let num = Math.floor(Math.random() * 5);
+
+//         document.getElementById("displayJ").innerHTML = "= " + arr[num]
+//     }
+//     else if (alpha === "K") {
+
+//         let arr = ["kite", "king", "key", "kangaroo", "koala"];
+//         let num = Math.floor(Math.random() * 5);
+
+//         document.getElementById("displayK").innerHTML = "= " + arr[num]
+//     }
+//     else if (alpha === "L") {
+
+//         let arr = ["lion", "laptop", "lamp", "leaf", "lemon"];
+//         let num = Math.floor(Math.random() * 5);
+
+//         document.getElementById("displayL").innerHTML = "= " + arr[num]
+//     }
+//     else if (alpha === "M") {
+
+//         let arr = ["monkey", "moon", "mountain", "milk", "map"];
+//         let num = Math.floor(Math.random() * 5);
+
+//         document.getElementById("displayM").innerHTML = "= " + arr[num]
+//     }
+//     else if (alpha === "N") {
+
+
+//         let arr = ["notebook", "net", "nose", "ninja", "necklace"];
+//         let num = Math.floor(Math.random() * 5);
+
+//         document.getElementById("displayN").innerHTML = "= " + arr[num]
+//     }
+//     else if (alpha === "O") {
+
+//         let arr = ["orange", "octopus", "owl", "ocean", "onion"];
+//         let num = Math.floor(Math.random() * 5);
+
+//         document.getElementById("displayO").innerHTML = "= " + arr[num]
+//     }
+//     else if (alpha === "P") {
+
+//         let arr = ["penguin", "piano", "peach", "pear", "pumpkin"];
+//         let num = Math.floor(Math.random() * 5);
+
+//         document.getElementById("displayP").innerHTML = "= " + arr[num]
+//     }
+//     else if (alpha === "Q") {
+
+//         let arr = ["queen", "quilt", "quail", "quarter", "question"];
+//         let num = Math.floor(Math.random() * 5);
+
+//         document.getElementById("displayQ").innerHTML = "= " + arr[num]
+//     }
+//     else if (alpha === "R") {
+
+//         let arr = ["rabbit", "rain", "rose", "rocket", "ring"];
+//         let num = Math.floor(Math.random() * 5);
+
+//         document.getElementById("displayR").innerHTML = "= " + arr[num]
+//     }
+//     else if (alpha === "S") {
+
+//         let arr = ["snake", "star", "sun", "sandwich", "sock"];
+//         let num = Math.floor(Math.random() * 5);
+
+//         document.getElementById("displayS").innerHTML = "= " + arr[num]
+//     }
+//     else if (alpha === "T") {
+
+//         let arr = ["tiger", "turtle", "tree", "table", "telescope"];
+//         let num = Math.floor(Math.random() * 5);
+
+//         document.getElementById("displayT").innerHTML = "= " + arr[num]
+//     }
+//     else if (alpha === "U") {
+
+//         let arr = ["umbrella", "unicorn", "uniform", "utensil", "undercover"]
+//         let num = Math.floor(Math.random() * 5);
+
+//         document.getElementById("displayU").innerHTML = "= " + arr[num]
+//     }
+//     else if (alpha === "V") {
+
+//         let arr = ["vase", "violin", "vegetable", "volcano", "van"];
+//         let num = Math.floor(Math.random() * 5);
+
+//         document.getElementById("displayV").innerHTML = "= " + arr[num]
+//     }
+//     else if (alpha === "W") {
+
+//         let arr = ["whale", "watermelon", "window", "watch", "wings"];
+
+//         let num = Math.floor(Math.random() * 5);
+
+//         document.getElementById("displayW").innerHTML = "= " + arr[num]
+//     }
+//     else if (alpha === "X") {
+
+
+//         let arr = ["xylophone", "x-ray", "xenon", "xerus", "xylem"];
+//         let num = Math.floor(Math.random() * 5);
+
+//         document.getElementById("displayX").innerHTML = "= " + arr[num]
+//     }
+//     else if (alpha === "Y") {
+
+//         let arr = ["yacht", "yogurt", "yarn", "yawn", "yellow"];
+//         let num = Math.floor(Math.random() * 5);
+
+//         document.getElementById("displayY").innerHTML = "= " + arr[num]
+//     }
+//     else if (alpha === "Z") {
+
+//         let arr = ["zebra", "zipper", "zoo", "zombie", "zigzag"];
+//         let num = Math.floor(Math.random() * 5);
+
+//         document.getElementById("displayZ").innerHTML = "= " + arr[num]
+//     }
+// }
+
+
+var show = document.getElementById("show");
+let button = document.querySelectorAll("button");
+let alphas = [];
+let words = [];
+let alpha = ["Apple", "Aeroplane", "Anger", "Alarm", "Actor", "banana", "basket", "bird", "book", "butterfly", "cat", "car", "cake", "cookie", "cloud", "dog", "duck", "door", "diamond", "dragon", "elephant", "egg", "earth", "eagle", "engine", "fish", "flower", "fire", "flag", "forest", "goat", "grass", "glass", "guitar", "gift", "horse", "hat", "house", "heart", "honey", "ice cream", "island", "igloo", "ink", "iron", "jellyfish", "jacket", "juice", "jam", "jet", "kite", "king", "key", "kangaroo", "koala", "lion", "laptop", "lamp", "leaf", "lemon", "monkey", "moon", "mountain", "milk", "map", "notebook", "net", "nose", "ninja", "necklace", "orange", "octopus", "owl", "ocean", "onion", "penguin", "piano", "peach", "pear", "pumpkin", "queen", "quilt", "quail", "quarter", "question", "rabbit", "rain", "rose", "rocket", "ring", "snake", "star", "sun", "sandwich", "sock", "tiger", "turtle", "tree", "table", "telescope", "umbrella", "unicorn", "uniform", "utensil", "undercover", "vase", "violin", "vegetable", "volcano", "van", "whale", "watermelon", "window", "watch", "wings", "xylophone", "x-ray", "xenon", "xerus", "xylem", "yacht", "yogurt", "yarn", "yawn", "yellow", "zebra", "zipper", "zoo", "zombie", "zigzag"];
+
+for (let i = 0; i < alpha.length; i++) {
+    var b = alpha[i][0].toUpperCase() + alpha[i].slice(1);
+
+    alphas.push(b)
+}
+
+button.forEach((button) => {
+
+    button.addEventListener("click", function () {
+
+        let btn = button.value.toUpperCase();
+        for (let alphabets of alphas) {
+            if (alphabets[0] === btn) {
+                words.push(alphabets)
+            }
+        }
+        if (words.length > 0) {
+            let index = Math.floor(Math.random() * words.length)
+            show.innerHTML = words[index];
+            let speak = window.speechSynthesis;
+            let sound =  speak.getVoices();
+            let voice = new SpeechSynthesisUtterance(`${words[index]}`);
+            voice.voice = sound[2];
+            voice.pitch = 1.8;
+            voice.rate = 2; 
+            speak.speak(voice)
+            words = [];
+        }
+    })
+});
+
+
+
+
+
+document.addEventListener("keydown", function (e) {
+    let key = e.key;
+    if ((key >= "A" && key <= "Z") || (key >= "a" && key <= "z")) {
+
+
+        for (let alphabets of alphas) {
+            if (alphabets[0].toLowerCase() == key.toLowerCase()) {
+                words.push(alphabets)
+            }
+        }
+        if (words.length > 0) {
+            let index = Math.floor(Math.random() * words.length)
+            show.innerHTML = words[index];
+            let speak = window.speechSynthesis;
+            let sound = speak.getVoices();
+            let voice = new SpeechSynthesisUtterance(`${words[index]}`);
+            voice.voice = sound[2];
+            voice.pitch =1.8;
+            voice.rate = 2 ;
+            speak.speak(voice)
+            words = [];
+        }
+    }
+
+})
+
+
+
+
+// document.addEventListener("keydown", function (e) {
+//     let key = e.key;
+//     if (key >= "A" && key <= "Z"|| key >= "a" && key <= "z") {
+//         for (let alphabet of alphas) {
+//             if (alphabet[0].toLowerCase() == key[0].toLowerCase()) {
+//                 words.push(alphabet)
+//             }
+//         }
+//         if (words.length > 0) {
+//             let num = Math.floor(Math.random() * words.length)
+//             show.innerHTML = words[num]
+//             let speak = window.speechSynthesis;
+//             let voice = new SpeechSynthesisUtterance(`${words[num]}`);
+//             speak.speak(voice)
+//             words = [];
+//         }
+//     }
+
+
+// })
